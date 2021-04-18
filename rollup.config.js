ab508e5c29cf4054f7a13fed8eaa588c4d479ed2
Uro3 <miyakawa.uro3@gmail.com>
2021-04-18 23:13:31 +0900

    use environment variables

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -39,6 +40,14 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		replace({
+      process: JSON.stringify({
+        env: {
+					CLIENT_ID: process.env.CLIENT_ID || 'abc',
+					REDIRECT_URI: process.env.REDIRECT_URI || 'http://localhost:5000'
+        }
+      }),
+    }),
 		svelte({
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import App from './App.svelte';
 
+const client_id = process.env.CLIENT_ID;
+const redirect_uri = process.env.REDIRECT_URI;
+
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		name: `world : ${client_id} : ${redirect_uri}`
 	}
 });
 
