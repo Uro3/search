5252f172f645fb41ee78be4488e15d7608338b5d
Uro3 <miyakawa.uro3@gmail.com>
2021-05-02 16:04:07 +0900

    add auth request

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,44 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let clientId: string;
 	export let redirectUri: string;
+	export let googleAuthEndpoint: string;
 	export let youtubeDataApiEndpoint: string;
 
+	let token: string;
+
 	const handleClick = async () => {
-		const response = await fetch(youtubeDataApiEndpoint);
+		if (!token) {
+			location.href = `${googleAuthEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=https://www.googleapis.com/auth/youtube.readonly`;
+			return;
+		}
+
+		const response = await fetch(youtubeDataApiEndpoint, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
 		const data = await response.json();
-		console.log(data)
+		console.log(data);
 	};
+
+	const parseHash = (hash: string) => {
+		return Object.fromEntries(hash.split('&').map(str => str.split('=')));
+	};
+
+	onMount(() => {
+		const parsedHash = parseHash(window.location.hash.substring(1));
+		token = parsedHash['access_token'] || '';
+
+		console.log('redirectUri: ', redirectUri);
+		console.log('clientId: ', clientId);
+		console.log('googleAuthEndpoint: ', googleAuthEndpoint);
+	});
 </script>
 
 <main>
 	<h1>Hello!</h1>
-	<ul>
-		<li>{clientId}</li>
-		<li>{redirectUri}</li>
-	</ul>
 	<button on:click={handleClick}>
 		hey!
 	</button>
